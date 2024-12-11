7f99c7392adee2fac55f2e809dced5540cf547b5
Even Stensberg <evenstensberg@gmail.com>
2024-12-11 14:26:09 +0100

    chore: push

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { IcClassificationBanner } from "@ukic/vue";
+</script>
+
+<template>
+    <div class="wrapper">
+      <IcClassificationBanner classification="official" inline="true" country="Norway" />
+    </div>
+</template>
+
+<style>
+@import "@ukic/fonts/dist/fonts.css";
+@import "@ukic/vue/dist/core/core.css";
+</style>
